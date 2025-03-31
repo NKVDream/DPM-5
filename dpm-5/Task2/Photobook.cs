ff0da60c092f461915be0e0f4407e70a13f396b2
NKVDream <Kmv235@yandex.ru>
2025-03-31 07:59:04 -0700

    2 done

---
+++ dpm-5/Task2/Photobook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class PhotoBook
+{
+    public string Photo1 { get; set; }
+    public string Photo2 { get; set; }
+    public int Date1 { get; set; }
+    public int Date2 { get; set; }
+    public bool Attitude {  get; set; }
+    public PhotoBook(string photo1, int date1)
+    {
+        Photo1 = photo1;
+        Date1 = date1;
+        Console.WriteLine($"you got only one photo.\t 1){photo1}:{date1}.");
+    }
+    public PhotoBook(string photo1, int date1, string photo2, int date2)
+    {
+        Photo1 = photo1;
+        Photo2 = photo2;
+        Date1 = date1;
+        Date2 = date2;
+        Console.WriteLine($"you got two photos.\t 1){photo1}:{date1}; 2){photo2}:{date2}.");
+    }
+    public PhotoBook(string photo1, int date1, bool attitude)
+    {
+        Photo1 = photo1;
+        Date1 = date1;
+        Attitude = attitude;
+        Console.WriteLine($"you got photo with mark.\t 1){photo1}:{date1}. Should i go there again?:{attitude}!");
+    }
+}
---
+++ dpm-5/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Task2
+{
+    static void Main()
+    {
+        PhotoBook photo1 = new PhotoBook("LA", 2018);
+        PhotoBook photo2 = new PhotoBook("Kyoto", 2020, "Hollywood", 2021);
+        PhotoBook photo3 = new PhotoBook("Muhosransk", 2022, false);
+    }
+}
